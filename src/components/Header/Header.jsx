ed3945d34e60207c5dc7534e288436eb8a031371
Adarsh Sahu <adarshsahu1077@gmail.com>
2024-01-30 17:04:14 +0530

    Added LogoutBtn

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,17 @@
 import React from 'react'
+import { Container, Logo, LogoutBtn } from '../index'
+import { Link } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
 
 function Header() {
+  const authStatus = useSelector((state) => state.auth.status)
+  const navigate = useNavigate()
+
+  const naItems = [
+    
+  ]
+
   return (
     <div>Header</div>
   )
---
+++ src/components/Header/LogoutBtn.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { useDispatch } from 'react-redux'
+import authService from '../../appwrite/config'
+import { logout } from '../../store/authSlice'
+
+function LogoutBtn() {
+    const dispatch = useDispatch() 
+    const logoutHandler = () => {
+        authService.logout().then(() => {
+            dispatch(logout())
+        })
+    }
+
+    return (
+        <button
+            className='px-6 py-2 inline-block duration-200 hover:bg-blue-100 rounded-full'
+        >Logout</button>
+    )
+}
+
+export default LogoutBtn
